3a94f4760391616a2a7ccb58c92d0a1a386cf77e
Asley Tan <asleytwh@gmail.com>
2018-04-19 11:35:16 +0800

    Compiled Javascript into A .JS

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+
+
+	function loginPls() {
+	document.getElementById('id01').style.display='table';
+};
+
+
+
+
+var modal = document.getElementById('id01');
+
+
+// Click on modal, close it
+window.onclick = function(event) {
+
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
