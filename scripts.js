95973ba0a479740b36bed51433aca3734aa8407f
Asley Tan <asleytwh@gmail.com>
2018-04-22 03:27:41 +0800

    Template animation and Navbar Animation smoothed out

--- scripts.js
+++ scripts.js
@@ -9,6 +9,8 @@
 
 
 var modal = document.getElementById('id01');
+var textone = document.getElementById('t1');
+var texttwo = document.getElementById('t2');
 
 
 // Click on modal, close it
@@ -17,4 +19,9 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function headerBreak() {
+	textone.style.display = "block";
+	texttwo.style.display = "block";
 }
